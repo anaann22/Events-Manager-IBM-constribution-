eba500a2d6c5ef75479c2f45d3d7b59fb93d27f4
irinafelea <irina.felea02@e-uvt.ro>
2023-05-06 01:05:55 +0300

    pagina user

--- Client/app/src/App.js
+++ Client/app/src/App.js
@@ -1,14 +1,19 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './Components/Pages/LoginPage';
 import RegisterPage from './Components/Pages/RegisterPage';
+import UserPage from './Components/Pages/UserPage';
+import './Style/App.css';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<UserPage />} />
+
+        {/* <Route path="/" element={<LoginPage />} /> */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/user" element={<UserPage />} />
       </Routes>
     </Router>
   );
