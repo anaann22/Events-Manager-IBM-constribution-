bc22aab7878a0aac30b02a947319e85c95039941
irinafelea <irina.felea02@e-uvt.ro>
2023-05-06 01:07:19 +0300

    user

--- Client/app/src/Components/Atoms/UserButton.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const UserButton = () => {
-    return <button></button>;
-  };
-
-export default UserButton;
--- Client/app/src/Components/Organisms/Header.js
+++ Client/app/src/Components/Organisms/Header.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import '../../Style/Header.css';
 import logo from '../../Images/logo.png'
-import UserButton from "../Atoms/UserButton"
 
 const Header = () => {
   return (
     <div className= "container">
       <img src={logo} />
       <h2 className= "title">Company Social Events</h2>
-      <div className='userButton'><UserButton/></div>
     </div>
   );
 };
