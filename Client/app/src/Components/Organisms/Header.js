0bd012c2158617541508b4aecee5e3976134ce04
irinafelea <irina.felea02@e-uvt.ro>
2023-05-07 01:06:10 +0300

    dropdown

--- Client/app/src/Components/Organisms/Header.js
+++ Client/app/src/Components/Organisms/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import '../../Style/Header.css';
-import logo from '../../Images/logo.png';
+import logo from '../../Images/logo2.png';
 import User from './../Molecules/UserDropDown';
 
 const Header = () => {
   return (
     <div className= "container">
-      <img alt="" src={logo} />
+      <img className="logo" alt="" src={logo} />
       <h2 className= "title">Company Social Events</h2>
       <User></User>
     </div>
