9dd3c3c43a189c2fff8229867888def20bec8836
ale <alexandra.cojocari02@e-uvt.ro>
2023-05-14 17:36:27 +0300

    gata intro

--- Client/src/Components/Organisms/IntroCard.js
+++ Client/src/Components/Organisms/IntroCard.js
@@ -5,10 +5,17 @@ import { Link } from 'react-router-dom';
 import '../../Style/IntroCard.css';
 
 const ProfileCard = () => {
+
   return (
     <div className="card">
       <div className="card-header">
         <h2 className="card-title">Welcome to your company event management! </h2>
+      </div>
+      <div className='descriere'>
+        To see the events you will need to log with your company credentials. 
+      </div>
+      <div className='descriere'>
+        If you don't have an account, please go to the register<br/> page and use your company email.
       </div>
       <div className="card-body">
         <div className="profile-details">
